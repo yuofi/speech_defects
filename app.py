6d3487b9a7abf2f550abf90b6af383c939260b3a
Pavel Yulov <yufii@users.noreply.huggingface.co>
2024-10-14 11:23:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app = FastAPI(port=8000)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],#origins,
+    allow_origins=["http://localhost:3000"],#origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
